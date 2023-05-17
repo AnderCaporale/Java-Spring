b3d174da6a174d01fddbc4baf9f3b3803c258f41
Anderson Caporale Anan <andersoncaporale@outlook.com>
2023-05-17 19:18:23 -0300

    Game Repository, Service, Controller, DTO

---
+++ dslist/src/main/java/com/anderson/dslist/services/GameService.java
@@ -0,0 +1,23 @@
+package com.anderson.dslist.services;
+
+import com.anderson.dslist.dto.GameMinDTO;
+import com.anderson.dslist.entities.Game;
+import com.anderson.dslist.repositories.GameRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+    
+    @Autowired
+    private GameRepository gameRepository;
+    
+    public List<GameMinDTO> findAll(){
+        List<Game> result = gameRepository.findAll();
+        
+        //Transforma a lista com a classe Game para uma lista com a classe GameMinDTO
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+}
